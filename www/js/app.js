27d1000e73630980dc114c923d36a4c580fafbc9
jzlshitian <243777820@qq.com>
2016-02-02 12:46:52 +0800

    2016-02-02

---
+++ www/js/app.js
@@ -0,0 +1,96 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  //使得导航栏在不同平台中适配标准
+  $ionicConfigProvider.platform.ios.tabs.style('standard');
+  $ionicConfigProvider.platform.ios.tabs.position('bottom');
+  $ionicConfigProvider.platform.android.tabs.style('standard');
+  $ionicConfigProvider.platform.android.tabs.position('standard');
+
+  $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+  $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+  $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+  $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+  $ionicConfigProvider.platform.ios.views.transition('ios');
+  $ionicConfigProvider.platform.android.views.transition('android');
+
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+  .state('app.projects', {
+    url: '/projects',
+    abstract: true,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/projects.html',
+      }
+    }
+  })
+  .state('app.projects.me', {
+    url: '/me',
+    views: {
+      'me': {
+        templateUrl: 'templates/projects.me.html',
+        controller: 'ProjectsMeCtrl'
+      }
+    }
+  })
+  .state('app.projects.others', {
+    url: '/others',
+    views: {
+      'others': {
+        templateUrl: 'templates/projects.others.html',
+        controller: 'ProjectsOthersCtrl'
+      }
+    }
+  })
+  .state('app.notice', {
+      url: '/notice',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/notice.html'
+        }
+      }
+    })
+    .state('app.plans', {
+      url: '/plans',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/plans.html',
+          controller: 'PlansCtrl'
+        }
+      }
+    });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/projects/me');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,154 @@
+angular.module('starter.controllers', [])
+
+.factory('Projects', function() {
+      return {
+        load: function() {
+          var data = window.localStorage['projects'];
+          if(data) {
+            return angular.fromJson(data);
+          }
+          return [];
+        },
+        save: function(projects) {
+          window.localStorage['projects'] = angular.toJson(projects);
+        }
+      }
+    }
+)
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.loginModal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.loginModal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.loginModal.show();
+  };
+
+  //打开注册页面
+  $scope.register = function() {
+    alert("暂未实现");
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+.controller('ProjectsMeCtrl', function($scope, $ionicModal, Projects) {
+
+    $ionicModal.fromTemplateUrl('templates/create-project.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.projectModal = modal;
+    });
+
+    $scope.projects = [
+      {
+        id: 1,
+        imgUrl:"img/projects/ionic.png",
+        name:"Demo",
+        pageNumber:1,
+        description: "我是一个测试项目，我是测试项目1号"
+      },
+      {
+        id: 2,
+        imgUrl:"img/projects/ionic.png",
+        name:"example",
+        pageNumber:3,
+        description: "我是一个测试项目，我是测试项目2号"
+      }
+    ];
+
+    $scope.projects = Projects.load();
+
+    $scope.edit = function(id) {
+      alert("你要编辑"+id);
+    };
+
+    $scope.delete = function(id) {
+      var pj = $scope.projects;
+      $scope.projects = pj.slice(0, id-1).concat(pj.slice(id, pj.length));
+      Projects.save($scope.projects);
+    };
+
+    $scope.openNewProject = function() {
+      $scope.projectModal.show();
+    };
+
+    $scope.closeNewProject = function() {
+      $scope.projectModal.hide();
+    };
+
+    $scope.createProject = function(project) {
+      if(project) {
+        $scope.projects.push(
+            {
+              id : $scope.projects.length+1,
+              imgUrl:"img/projects/ionic.png",
+              name: project.name,
+              pageNumber: 0,
+              description: project.description
+            }
+        );
+        $scope.projectModal.hide();
+        Projects.save($scope.projects);
+        $scope.name = "";
+        $scope.description = "";
+      }
+    };
+})
+.controller('ProjectsOthersCtrl', function($scope) {
+  $scope.projects = [
+    {
+      id: 1,
+      imgUrl:"img/projects/ionic.png",
+      name:"DemoOthers",
+      pageNumber:1,
+      description: "我是一个测试项目，我是测试项目3号"
+    },
+    {
+      id: 2,
+      imgUrl:"img/projects/ionic.png",
+      name:"exampleOthers",
+      pageNumber:31,
+      description: "我是一个测试项目，我是测试项目1号"
+    }
+  ];
+})
+.controller('PlansCtrl', function($scope) {
+  $scope.plans = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+});
