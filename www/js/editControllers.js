35cef10962ec4c4cf4914668915116021b571892
jzlshitian <243777820@qq.com>
2016-02-03 16:23:48 +0800

    create and delete proect

--- www/js/editControllers.js
+++ www/js/editControllers.js
@@ -4,27 +4,77 @@
  */
 angular.module('starter.editControllers', [])
 
-  .controller('ProjectsMeCtrl', function($scope) {
-    $scope.projects = [
-      {
-        id: 1,
-        imgUrl:"img/projects/ionic.png",
-        name:"Demo",
-        pageNumber:1,
-        description: "我是一个测试项目，我是测试项目1号"
+  .factory('Projects', function() {
+    return {
+      load: function() {
+        var data = window.localStorage['projects'];
+        if(data) {
+          return angular.fromJson(data);
+        }
+        return [];
       },
-      {
-        id: 2,
-        imgUrl:"img/projects/ionic.png",
-        name:"example",
-        pageNumber:3,
-        description: "我是一个测试项目，我是测试项目2号"
+      save: function(projects) {
+        window.localStorage['projects'] = angular.toJson(projects);
       }
-    ];
+    }
+  })
+
+  .controller('ProjectsMeCtrl', function($scope, $ionicModal, $ionicPopup, Projects) {
+    $ionicModal.fromTemplateUrl('templates/create-project.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.projectModal = modal;
+    });
+
+    $scope.projects = Projects.load();
 
     $scope.edit = function(id) {
-      //alert("你要编辑"+id);
       window.location.href = "#app/projectDetail";
+    };
+
+    $scope.delete = function(id) {
+      var popup = $ionicPopup.confirm({
+        title: '删除项目',
+        template: '你确定要删除吗?',
+      });
+      popup.then(function(res) {
+        if(res){
+          var pj = $scope.projects;
+          if(pj.length!=1) {
+            $scope.projects = pj.slice(0, id-1).concat(pj.slice(id, pj.length));
+          }
+          else {
+            $scope.projects = [];
+          }
+          Projects.save($scope.projects);
+        }
+      })
+    };
+
+    $scope.openNewProject = function() {
+      $scope.projectModal.show();
+    };
+
+    $scope.closeNewProject = function() {
+      $scope.projectModal.hide();
+    };
+
+    $scope.createProject = function(project) {
+      if(project) {
+        $scope.projects.push(
+            {
+              id : $scope.projects.length+1,
+              imgUrl:"img/projects/ionic.png",
+              name: project.name,
+              pageNumber: 0,
+              description: project.description
+            }
+        );
+        $scope.projectModal.hide();
+        Projects.save($scope.projects);
+        project.name = "";
+        project.description = "";
+      }
     };
   })
   .controller('ProjectsOthersCtrl', function($scope) {
