86bd727b9c89f4a53f596ac349bd22d40c8f2dcf
N4A <181387406@qq.com>
2016-03-06 16:20:09 +0800

    添加图片

--- www/js/editControllers.js
+++ www/js/editControllers.js
@@ -2,7 +2,7 @@
  * Created by duocai on 2016/1/30.
  * 编辑项目控制器
  */
-angular.module('starter.editControllers', [])
+angular.module('starter.editControllers', ['ngCordova'])
 
   .factory('Projects', function() {
     return {
@@ -212,17 +212,35 @@ angular.module('starter.editControllers', [])
       ];
   })
 
-  .controller('projectDetailCtrl',function($scope, $rootScope) {
+  .controller('projectDetailCtrl',function($scope, $rootScope,$cordovaCamera) {
 
     $scope.projectImagesToShow = [];
 
-    for (var i = 0; i<$rootScope.curpj.images.length/3; i++) {
-      $scope.projectImagesToShow.push([
-        $rootScope.curpj.images[i*3],
-        $rootScope.curpj.images[i*3+1],
-        $rootScope.curpj.images[i*3+2]
-      ]);
-    }
+    $scope.setShow = function () {
+      $scope.projectImagesToShow = [];
+      for (var i = 0; i<$rootScope.curpj.images.length/3; i++) {
+        if (i * 3 + 2 < $rootScope.curpj.images.length) {
+          $scope.projectImagesToShow.push([
+            $rootScope.curpj.images[i * 3],
+            $rootScope.curpj.images[i * 3 + 1],
+            $rootScope.curpj.images[i * 3 + 2]
+          ]);
+        }
+        else if (i * 3 + 1 < $rootScope.curpj.images.length) {
+          $scope.projectImagesToShow.push([
+            $rootScope.curpj.images[i * 3],
+            $rootScope.curpj.images[i * 3 + 1]
+          ]);
+        }
+        else {
+          $scope.projectImagesToShow.push([
+            $rootScope.curpj.images[i * 3]
+          ]);
+        }
+      }
+    };
+
+    $scope.setShow();
 
     $scope.edit = function(id) {
       $rootScope.curImg = $rootScope.curpj.images[id];
@@ -233,7 +251,32 @@ angular.module('starter.editControllers', [])
       window.location.href="#app/projectOperation";
     };
 
-  })
+    $scope.takePhoto=function() {
+      var options = {
+        quality: 100,                                            //相片质量0-100
+        destinationType: Camera.DestinationType.FILE_URI,        //返回类型：DATA_URL= 0，返回作为 base64 編碼字串。 FILE_URI=1，返回影像档的 URI。NATIVE_URI=2，返回图像本机URI (例如，資產庫)
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,             //从哪里选择图片：PHOTOLIBRARY=0，相机拍照=1，SAVEDPHOTOALBUM=2。0和1其实都是本地图库
+        allowEdit: false,                                        //在选择之前允许修改截图
+        encodingType: Camera.EncodingType.JPEG,                   //保存的图片格式： JPEG = 0, PNG = 1
+        mediaType: 0,                                             //可选媒体类型：圖片=0，只允许选择图片將返回指定DestinationType的参数。 視頻格式=1，允许选择视频，最终返回 FILE_URI。ALLMEDIA= 2，允许所有媒体类型的选择。
+        cameraDirection: 0,                                       //枪后摄像头类型：Back= 0,Front-facing = 1
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: true                                   //保存进手机相册
+      };
+
+      $cordovaCamera.getPicture(options).then(function (imagePath) {
+        $rootScope.curpj.images.push({
+          id: $rootScope.curpj.images.length,
+          path: imagePath,
+          boxes: []
+        });
+        $scope.setShow();
+        $scope.apply();
+      }, function (err) {
+        alert(err);
+      });
+    };
+    })
   .controller('projectEditCtrl', function($scope, $rootScope, $ionicPopover, Projects, $timeout) {
       $ionicPopover.fromTemplateUrl('templates/popover.html', {
         scope: $scope,
